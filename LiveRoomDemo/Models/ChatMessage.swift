beea31d4dc6c228308ca51b19fb722d014cbe6b4
amoving666-beep <amoving666@gmail.com>
2026-06-01 20:04:20 +0800

    初始化 LiveRoomDemo 工程结构

---
+++ LiveRoomDemo/Models/ChatMessage.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessage.swift
+//  LiveRoomDemo
+//
+//  Created by 天亮了 on 2026/6/1.
+//
+
+import Foundation
---
+++ LiveRoomDemo/Modules/LiveRoom/View/LiveRoomHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  LiveRoomHeaderView.swift
+//  LiveRoomDemo
+//
+//  Created by 天亮了 on 2026/6/1.
+//
+
+import Foundation
